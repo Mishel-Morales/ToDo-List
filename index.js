adfa52c7a827bae1c0a74ca873654bf677c26aac
Mishel Morales <mmejiam1825@gmail.com>
2025-05-12 16:23:47 -0600

    Agregando en index.html select e input, index.js agregando cambios y un cambio a index.css

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
 let botton = document.querySelector('#agregarBtn')
 let ul = document.querySelector('ul')
-let input = document.querySelector('#tareaInput')
+let inputTask = document.querySelector('#tareaInput')
+let filtro = document.querySelector('.filtro')
+
+let tareas = []
 
 const agregar = (event) => {
-    let inputTask = document.querySelector('#tareaInput')
+    let nuevaTarea = {
+        texto : inputTask.value,
+        isCompleted : false
+    }
+
     let li = document.createElement('li')
         li.classList.add ('list-unstyled', 'ms-5')
     let div = document.createElement('div')
     let input = document.createElement('input')
-        input.value = inputTask.value
+        input.value = nuevaTarea.texto
         input.classList.add ('p-1', 'rounded', 'w-75')
         inputTask.value = ''
     let buttonOne = document.createElement('button')
@@ -16,12 +23,21 @@ const agregar = (event) => {
         buttonOne.className = 'bg-danger m-1 p-1 rounded'
         buttonOne.addEventListener('click', (event) =>{
             li.remove()
+            tareas = tareas.filter(item => item.texto != event.target.previousElementSibling.value)
+            console.log(tareas)
         })
-    let buttonTwo = document.createElement('button')
+    let buttonTwo = document.createElement('button')  
         buttonTwo.innerText = 'Finalizado'
         buttonTwo.className = 'p-1 rounded'
         buttonTwo.addEventListener('click', (event) => {
              input.classList.add("text-decoration-line-through")
+             tareas = tareas.filter(item => {
+                if(item.texto == event.target.previousElementSibling.previousElementSibling.value){
+                    item.isCompleted = true;
+                }
+                return item
+             })
+             console.log(tareas)
         })
    
     li.appendChild(div)
@@ -29,19 +45,75 @@ const agregar = (event) => {
     div.appendChild(buttonOne)
     div.appendChild(buttonTwo)
     ul.appendChild(li)
+
+    tareas.push(nuevaTarea)
+    console.log(tareas)
 }
 
+
+const filtroTareas = (value) => {
+    let filtro = []
+    if(value == 'Pendientes'){
+        filtro = tareas.filter(item => item.isCompleted == false)
+    }  else if(value == 'Completados'){
+        filtro = tareas.filter(item => item.isCompleted == true)
+    } else{
+        filtro = tareas
+    }
+
+    let selectfilter = ''
+    ul.innerHTML =''
+    filtro.forEach(item => {
+        let li = document.createElement('li')
+        li.classList.add ('list-unstyled', 'ms-5')
+    let div = document.createElement('div')
+    let input = document.createElement('input')
+        input.value = item.texto
+        input.classList.add ('p-1', 'rounded', 'w-75')
+        inputTask.value = ''
+    let buttonOne = document.createElement('button')
+        buttonOne.innerText = 'Eliminar'
+        buttonOne.className = 'bg-danger m-1 p-1 rounded'
+        buttonOne.addEventListener('click', (event) =>{
+            li.remove()
+            tareas = tareas.filter(item => item.texto != event.target.previousElementSibling.value)
+            console.log(tareas)
+        })
+    let buttonTwo = document.createElement('button')  
+        buttonTwo.innerText = 'Finalizado'
+        buttonTwo.className = 'p-1 rounded'
+        buttonTwo.addEventListener('click', (event) => {
+             input.classList.add("text-decoration-line-through")
+             tareas = tareas.filter(item => {
+                if(item.texto == event.target.previousElementSibling.previousElementSibling.value){
+                    item.isCompleted = true;
+                }
+                return item
+             })
+             console.log(tareas)
+        })
+   
+    li.appendChild(div)
+    div.appendChild(input)
+    div.appendChild(buttonOne)
+    div.appendChild(buttonTwo)
+    ul.appendChild(li)
+
+    })
+}
+
+
 botton.addEventListener('click', () => {
-    if(input.value == ''){
+    if(inputTask.value == ''){
         alert('Debe ingresar una tarea')
     } else{
         agregar()
     }
 })
 
-input.addEventListener('keydown', (event) => {
+inputTask.addEventListener('keydown', (event) => {
     if(event.key == 'Enter'){
-        if(input.value == ''){
+        if(inputTask.value == ''){
             alert('Debe ingresar una tarea')
         } else{
             agregar()
@@ -49,3 +121,7 @@ input.addEventListener('keydown', (event) => {
     }
 })
 
+filtro.addEventListener('change', (event) => {
+    console.log(event.target.value)
+    filtroTareas(event.target.value)
+})
